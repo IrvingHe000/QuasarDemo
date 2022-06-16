101165177845a38c426938561c925c7ae29a301f
IrvingHe000 <heoutong@gmail.com>
2022-06-16 17:53:33 +0800

    First commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,110 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title> Quasar App </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-footer>
+      <q-tabs>
+      <q-route-tab v-for="nav in navs"
+          :key="nav.key"
+          :to="nav.to" 
+          :icon="nav.icon"
+          :label="nav.label"/>
+      </q-tabs>
+    </q-footer>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-1"
+    >
+      <q-list>
+        <q-item-label header class="text-grey-8"> Navbar </q-item-label>
+        <q-item 
+          
+          v-for="nav in navs"
+          :key="nav.key"
+          :to="nav.to" 
+          exact
+          clickable>
+          <q-item-section avatar>
+            <q-icon :name="nav.icon" />
+          </q-item-section>
+
+          <q-item-section avatar>
+            <q-item-label>{{ nav.label }}</q-item-label>
+          </q-item-section>
+
+        </q-item>
+
+  
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { openURL } from "quasar";
+
+export default {
+  name: "MainLayout",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+
+    caption: {
+      type: String,
+      default: "",
+    },
+
+    link: {
+      type: String,
+      default: "#",
+    },
+
+    icon: {
+      type: String,
+      default: "",
+    },
+  },
+
+  data() {
+    return {
+      leftDrawerOpen: this.$q.platform.is.desktop,
+      navs: [
+        {label: 'Todo',
+          icon: 'list',
+          to: '/pagetodo',
+          key: '1'
+        },
+        {label: 'Settings',
+          icon: 'settings',
+          to: '/setting',
+          key: '2'
+        }
+      ]
+    }
+  }
+}
+</script>
---
+++ src/router/routes.js
@@ -0,0 +1,21 @@
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/Index.vue') },
+      { path: '/setting', component: () => import('pages/Setting.vue') },
+      { path: '/pagetodo', component: () => import('pages/PageTodo.vue') }
+    ]
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '*',
+    component: () => import('pages/Error404.vue')
+  }
+]
+
+export default routes
