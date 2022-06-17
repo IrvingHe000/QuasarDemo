6ceaf8d3d904b917d6b05e57622577baef68d7e5
IrvingHe000 <heoutong@gmail.com>
2022-06-17 14:41:01 +0800

    add task list

--- src/pages/PageTodo.vue
+++ src/pages/PageTodo.vue
@@ -1,11 +1,67 @@
 <template>
-  <q-page class="flex flex-center">
-    <h5>PageTodo</h5>
+  <q-page class="q-pa-md">
+    <q-list bordered
+    separator>
+
+      <q-item v-ripple
+          @click="task.completed = !task.completed" v-for="task in tasks"
+          :key="task.id" clickable :class="!task.completed ? 'bg-orange-1' : 'bg-green-1'"
+      >
+        <q-item-section side top>
+          <q-checkbox v-model="task.completed" />
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label
+            :class="{'text-strike' : task.completed}">{{ task.name }}</q-item-label>
+
+        </q-item-section>
+
+        <q-item-section side>
+          <div class="row">
+            <div class="column justify-center">
+              <q-icon name="event" color="grey" size="18px" class="q-mr-xs"/>
+            </div>
+            <div class="column">
+          <q-item-label class="row justify-end" caption>{{ task.dueDate }}</q-item-label>
+          <q-item-label class="row justify-end" caption><small>{{ task.dueTime }} </small></q-item-label>
+            </div>
+          </div>
+        </q-item-section>
+      </q-item>
+
+    </q-list>
   </q-page>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          name: 'Go to shop',
+          completed: false,
+          dueDate: '2019/05/12',
+          dueTime: '18:30'
+        },
+        {
+          id: 2,
+          name: 'Get apples',
+          completed: false,
+          dueDate: '2019/05/12',
+          dueTime: '18:30'
+        },
+        {
+          id: 3,
+          name: 'Get apples',
+          completed: false,
+          dueDate: '2019/05/12',
+          dueTime: '18:30'
+        }
+      ]
+    }
+  }
 }
 </script>
